b8c427cf5b59a18a8d4a3d3e65cff2f373b63e0f
Minh anh LE DUC <mle-duc@paul-f4Ar3s1.clusters.42paris.fr>
2024-01-19 20:40:04 +0100

    cleaned ex00's main

--- ex00/Animal.hpp
+++ ex00/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: mle-duc <mle-duc@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 23:06:19 by mle-duc           #+#    #+#             */
-/*   Updated: 2024/01/07 23:43:31 by mle-duc          ###   ########.fr       */
+/*   Updated: 2024/01/19 18:54:10 by mle-duc          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,10 +27,7 @@ class Animal
 	Animal &operator=(const Animal &src);
 	virtual void	makeSound(void) const;
 
-	//setter
 	void	setType(std::string type);
-
-	//getter
 	std::string	getType(void) const;
 
 	protected:
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mle-duc <mle-duc@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 23:40:25 by mle-duc           #+#    #+#             */
-/*   Updated: 2024/01/07 23:40:35 by mle-duc          ###   ########.fr       */
+/*   Updated: 2024/01/19 20:39:32 by mle-duc          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,96 +16,56 @@
 #include "WrongAnimal.hpp"
 #include "WrongCat.hpp"
 
+const std::string red("\033[0;31m");
+const std::string green("\033[1;32m");
+const std::string yellow("\033[1;33m");
+const std::string cyan("\033[0;36m");
+const std::string magenta("\033[0;35m");
+const std::string reset("\033[0m");
+
 int main()
 {
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const Animal	*meta = new Animal();
-	std::cout << std::endl;
-	std::cout << "\033[34mTesting\033[0m" << std::endl;
 
-	std::cout << "Animal _type: " << meta->getType() << std::endl;
-	meta->makeSound();
-	std::cout << std::endl;
+	std::cout << magenta << "_________________________________________________________\n\n|\t\t\tAnimal tests\t\t\t|\n_________________________________________________________\n" << reset << std::endl;
+	std::cout << magenta << "Creating one instance of each class : " << reset << std::endl;
+	const Animal	*animal = new Animal();
+	const Animal	*dog = new Dog();
+	const Animal	*cat = new Cat();
 
-	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	delete meta;
+	std::cout << red << "animal->getType() : " << reset << animal->getType() << std::endl;
+	std::cout << red << "dog->getType() : " << reset << dog->getType() << std::endl;
+	std::cout << red << "cat->getType() : " << reset << cat->getType() << std::endl;
 	std::cout << std::endl;
-
-	std::cout << "-------------------------------------------------------" << std::endl;
-
-	std::cout << std::endl;
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const Animal	*catto = new Cat();
-	std::cout << std::endl;
-
-	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	std::cout << "Cat _type: " << catto->getType() << std::endl;
-	catto->makeSound();
+	std::cout << red << "animal->makeSound() : " << reset;
+	animal->makeSound();
+	std::cout << red << "dog->makeSound() : " << reset;
+	dog->makeSound();
+	std::cout << red << "cat->makeSound() : " << reset;
+	cat->makeSound();
 	std::cout << std::endl;
 
-	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	delete catto;
+	std::cout << cyan << "Deleting the instances" << reset << std::endl;
+	delete animal;
+	delete dog;
+	delete cat;
 	std::cout << std::endl;
 
-	std::cout << "-------------------------------------------------------" << std::endl;
+std::cout << magenta << "_________________________________________________________________\n\n|\t\t\tWrongAnimal tests\t\t\t|\n_________________________________________________________________\n" << reset << std::endl;
+	std::cout << magenta << "Creating an instance of WrongAnimal and WrongCat: " << reset << std::endl;
+	const WrongAnimal	*wrong_animal = new WrongAnimal();
+	const WrongAnimal	*wrong_cat = new WrongCat();
 
-	std::cout << std::endl;
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const Animal	*doggo = new Dog();
-	std::cout << std::endl;
-	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	std::cout << "Dog _type: " << doggo->getType() <<std::endl;
-	doggo->makeSound();
-	std::cout << std::endl;
-	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	delete doggo;
-	std::cout << std::endl;
-
-	std::cout << "-------------------------------------------------------" << std::endl;
-
-	std::cout << std::endl;
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const WrongAnimal	*wrong_meta = new WrongAnimal();
+	std::cout << red << "wrong_animal->getType(): " << reset << wrong_animal->getType() << std::endl;
+	std::cout << red << "wrong_cat->getType() : " << reset << wrong_cat->getType() << std::endl;
+	std::cout << red << "animal->makeSound() : " << reset;
+	wrong_animal->makeSound();
+	std::cout << red << "wrong_cat->makeSound() : " << reset;
+	wrong_cat->makeSound();
 	std::cout << std::endl;
 
-	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	std::cout << "Animal _type: " << wrong_meta->getType() << std::endl;
-	wrong_meta->makeSound();
+	std::cout << cyan << "Deleting the instances" << reset << std::endl;
+	delete wrong_animal;
+	delete wrong_cat;
 	std::cout << std::endl;
-
-	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	delete wrong_meta;
-	std::cout << std::endl;
-
-	std::cout << "-------------------------------------------------------" << std::endl;
-
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const WrongAnimal	*wrong_catto = new WrongCat();
-	std::cout << std::endl;
-
-	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	std::cout << "WrongCat _type: " << wrong_catto->getType() <<std::endl;
-	wrong_catto->makeSound();
-	std::cout << std::endl;
-
-	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	delete wrong_catto;
-	std::cout << std::endl;
-
-	std::cout << "-------------------------------------------------------" << std::endl;
-
-	std::cout << "\033[34mConstructing\033[0m" << std::endl;
-	const WrongCat	*wrong_catta = new WrongCat();
-	std::cout << std::endl;
-
-	std::cout << "\033[34mTesting\033[0m" << std::endl;
-	std::cout << "WrongCat _type: " << wrong_catta->getType() <<std::endl;
-	wrong_catta->makeSound();
-	std::cout << std::endl;
-
-	std::cout << "\033[34mDeconstructing\033[0m" << std::endl;
-	delete wrong_catta;
-	std::cout << std::endl;
-
 	return (0);
 }
